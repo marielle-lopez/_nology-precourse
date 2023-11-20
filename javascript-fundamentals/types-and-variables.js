556d85137ce93b3f0d3157ecff752110c5820f88
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2023-11-20 11:11:57 +1100

    completed types and variables introduction

---
+++ javascript-fundamentals/types-and-variables.js
@@ -0,0 +1,12 @@
+typeof "Hello world!"       // "string"
+typeof 22       // "number"
+
+const myName = "Marielle"       // declaring and initialising variable
+myName      // "Marielle"
+
+const myArray = ["Marielle", 22]
+myArray[1]      // 22
+
+const fullName = { firstName: "Marielle", middleName: "Louisse", lastName: "Lopez" }        // declaring and initialising object
+fullName["firstName"]       // "Marielle"
+fullName.middleName         // "Louisse"
