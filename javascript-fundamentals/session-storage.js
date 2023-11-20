ab2642cf3e7902cbfcabc025993d17f4a5e93b9a
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2023-11-20 11:03:11 +1100

    completed session storage introduction

---
+++ javascript-fundamentals/session-storage.js
@@ -0,0 +1,9 @@
+sessionStorage.setItem('firstName', 'Marielle'); 
+sessionStorage.getItem('firstName');
+
+sessionStorage.setItem('lastName', 'Lopez');
+sessionStorage.getItem('lastName');
+
+sessionStorage.getItem('firstName') + ' ' + sessionStorage.getItem('lastName');
+
+sessionStorage.clear()      // clears data in sessionStorage
